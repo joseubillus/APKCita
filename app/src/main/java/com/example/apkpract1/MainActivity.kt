aec95879dc320341c5ff257a28c561cef7401126
joseubillus <jose_ubillus@hotmail.com>
2025-03-19 16:18:19 -0500

    Initial commit

---
+++ app/src/main/java/com/example/apkpract1/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.apkpract1
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import android.view.View
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun OnClick_Btnacep(v:View){
+        startActivity(Intent(this,MnLogin::class.java))
+    }
+}
